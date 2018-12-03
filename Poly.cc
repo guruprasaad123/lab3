6d9784e852ce42640a7e5328b53362b4c08c91b6
guruprasaad123 <jiiva116@live.com>
2018-12-03 22:41:37 +0530

    version 1.0

--- Poly.cc
+++ Poly.cc
@@ -1,27 +1,68 @@
 #include<iostream>
 #include "Poly.h"
+#include<cmath>
+
 #define MAX_SIZE 10
 
 using namespace std;
 
-Poly::Poly()
+
+// helper or Utility Function to minize the possibility of error in case of zero polynomial
+int OptimizeOrder(const Vector& vector)
 {
-    values=NULL;
-    size=0;
+     int order=vector.getSize()-1;
+     while( order>0 && vector(order)==0) 
+     {
+          order--; 
+     }    
+     return order;  
+}
+// helper or Utility Function to initialie the vector with the value of {0}
+Vector initWithZeros(const int & size)
+{
+    int i=0;
+    Vector vector(size);
+    for(;i<vector.getSize();i++)
+    {
+    vector(i) = 0.;    
+    }
+    return vector;
+}
+ /*Poly::Poly(const Poly & ref)
+{
+poly=ref;
+
+}
+Poly::~Poly()
+{
+poly(0);
 }
 
-Poly::Poly(double val[],int N)
+*/
+
+
+Poly::Poly(const double *val,const int &N):poly(N-1)         
 {
-    size=N;
-    int i=0;
-    double *array;
+
     try{
-       array = new double[N];
-       values=array;
-       for(;i<size;i++)
-       {
-           array[i]=val[i];
-       }
+
+     if(N<0)
+     {
+        cout<<"Sorry, not defined! Order of the polynomial has to be greater than or equal to zero!";
+        cout<<endl;
+        throw -1;
+     }
+     if(N!=0 && val[N]==0 ) 
+     {
+        cout<<"Invalid! The last coefficient of polynomials(order > 0) cannot be zero!";
+        cout<<endl;
+        throw -1;   
+     }      
+     for(int i=0;i<N;i++)
+     {
+         poly(i) = val[i];
+     } 
+
     }
     catch(...)
     {
@@ -32,90 +73,175 @@ Poly::Poly(double val[],int N)
 
 void Poly::print(void) const
 {
+    int size = poly.getSize();
+    cout<<"poly(x)=";
     try{
-if( size > 0)
-{
+    if( size > 0)
+    {
     
-    if( values[0]!=0 )
+    if( poly(0)!=0 )
     {
-        cout<<values[0]<<"+";
+        cout<<poly(0);
     }
 
-int i=0;
+    int i=1;
 
-for(;i<size;i++)
-{
-string end=(i==size-1)?"":"+";
-if( values[i]!=0 )
-{
-    cout<<values[i]<<"*x^"<<i+1<<end;
-}
+    for(;i<size;i++)
+    {
+    string end=(i==size-1)?"":"+";
+    if( poly(i)!=0 )
+    {
+    string end = (poly(i)>0)?"+":"";
+    cout<<end;
+    cout<<poly(i)<<"*x^"<<i;
+    }
 
-}
-cout<<endl;
-}
+    }
+    cout<<endl;
+    }
     }
     catch(...)
     {
         throw -1;
     }
-
 }
 
-int Poly::getSize(void) const 
+
+double Poly::operator()(const double &var) const
 {
-return size;
+int i=0,size=poly.getSize();
+double res=0;
+try{
+    for(;i<size;i++)
+    {
+        res+= (poly(i)*pow(var,i));
+       // cout<<"pow > "<< (values[i]*pow(var,i))<<endl;
+    }
+}
+catch(...)
+{
+    throw -1;
+}
+return res;
 }
 
-Poly& Poly::operator+(const Poly & opp)
-{
+Poly Poly::operator+(const Poly & opp) const
+{ 
+    int max_size =( poly.getSize() > opp.poly.getSize() )? poly.getSize():opp.poly.getSize();
+    int i=0;
+    Vector newVector(max_size+1);
+ 
     try{
-int min_size = ( size < opp.getSize() ) ? size:opp.getSize();
- int i=0;
- for(;i<min_size;i++)
- {
-     values[i] = values[i] + opp.values[i];
- }
+        newVector = ( poly.getSize()-1 == max_size ) ? ( poly+opp.poly ):( opp.poly+poly );
+
     }
     catch(...)
     {
         throw -1;
     }
- return *this;
+    int optorder = OptimizeOrder(newVector);
+    Poly newPoly(&newVector(0),optorder);
 }
 
-
-Poly& Poly::operator-(const Poly & opp)
+void Poly::operator=(const Poly&) const
 {
-   try{
-int min_size = ( size < opp.getSize() ) ? size:opp.getSize();
- int i=0;
- for(;i<min_size;i++)
- {
-     values[i] = values[i] * opp.values[i];
- }
-    }
-    catch(...)
-    {
-        throw -1;
-    }
- return *this;
+     cout<<"Sorry, assignment(=) operator is not available for use with polynomials";
+     cout<<endl;
+     throw -1;
 }
 
 
-Poly& Poly::operator*(const Poly & opp)
+Poly Poly::operator-(const Poly & opp) const
 {
-   try{
-int min_size = ( size < opp.getSize() ) ? size:opp.getSize();
- int i=0;
- for(;i<min_size;i++)
- {
-     values[i] = values[i] * opp.values[i];
- }
+     int max_size =( poly.getSize() > opp.poly.getSize() )? poly.getSize():opp.poly.getSize();
+    int i=0;
+    Vector newVector(max_size+1);
+ 
+    try{
+        newVector = ( poly.getSize()-1 == max_size ) ? ( poly-opp.poly ):( opp.poly-poly );
+
     }
     catch(...)
     {
         throw -1;
     }
- return *this;
+    int optorder = OptimizeOrder(newVector);
+    Poly newPoly(&newVector(0),optorder);
+    return newPoly;
 }
+
+
+
+Poly Poly::operator*(const Poly & opp) const
+{
+    int order =( opp.poly.getSize()-1 ) + ( poly.getSize()-1);
+    int i=0;
+   // Vector ZeroVector(order+1);
+    Vector tempVector(order+1);
+    Vector ZeroVector=initWithZeros(order+1);
+  
+    try{
+        for(;i<poly.getSize();i++)
+        {
+            tempVector*=0.;
+            int j=0;
+            for(;j<opp.poly.getSize();j++)
+            {
+                tempVector(i+j) = poly(i) * opp.poly(j);
+            }
+            ZeroVector+=tempVector;
+        }
+    }
+    catch(...)
+    {
+        throw -1;
+    }
+    int optorder = OptimizeOrder(ZeroVector);
+    Poly newPoly(&ZeroVector(0),optorder);
+ return newPoly;
+}
+
+
+//Euclidean Divison of polynomials to find the quotient
+Poly Poly::euclidienDivisionQ(const Poly& Divisor) const
+{
+     int n1=poly.getSize()-1; int n2=Divisor.poly.getSize()-1;
+     if(n2==0 && Divisor.poly(n2)==0)  //Ensuring the divisor is not a zero polynomial ( p(x) = 0 )
+     {
+         cout<<"Invalid! Divisor cannot be a zero polynomial!"<<endl;
+         throw -1; 
+     }
+     if(n2>n1)  //Ensuring the order of the divisor is not greater than that of the dividend
+     {
+         cout<<"Not defined!, please make sure the order of the divisor is not greater than the order of the dividend!"<<endl;
+         throw -2;
+     }
+     Poly Num(*this); //Making a copy of the dividend to Num, to be able to modify Num without modifying the input dividend polynomial
+     int i = (n1-n2);   //Order of the resulting quotient polynomial
+     Vector vQ=initWithZeros(i+1);  //Vector of the Quotient polynomial with zeros initially
+     Vector vR=initWithZeros(n1+1);     //Vector of the remainder polynomial with zeros initially
+     Vector temp=initWithZeros(n1+1);  //Temporary vector for an intermediate operation
+     while(n1>=n2)   //Repeat until the order of the numerator is >= to that of the denomerator
+     {
+         vQ(i)=Num.poly(n1)/Divisor.poly(n2);  //Divison of the highest degree coefficients of the numerator and the divisor
+         for(int j=n2; j>=0; j--)
+             temp(i+j)=vQ(i)*Divisor.poly(j);  //Multiplying each term of the divisor with the coefficient of the quotient corresponding to each step
+         Num.poly-=temp;  //New numerator after subtracting the result from previous step from the old numerator
+         temp*=0;   //Flushing the temp vector back to zeros before next iteration
+         n1--; i--;  //Decreasing the order of the numerator after each step, and decreasing the index of the quotient vector
+     }
+     return Poly(&vQ(0), vQ.getSize()-1); //Returning the quotient polynomial
+}
+
+//Euclidean Divison of polynomials to find the remainder (Version 1.0)
+Poly Poly::euclidienDivisionR(const Poly& Divisor) const //Remainder = Dividend - Quotient*Divisor
+{
+     return Poly((*this) - ((*this).euclidienDivisionQ(Divisor))*Divisor); //Function call to first find quotient of the inputs and then compute the remainder
+}
+
+//Euclidean Divison of polynomials to find the remainder (Version 2.0)
+Poly Poly::euclidienDivisionR(const Poly& Divisor, const Poly& Quotient) const //Remainder = Dividend - Quotient*Divisor
+{
+     return Poly((*this) - Quotient*Divisor);  //Use the previous Poly operator overloading functions with the given inputs to compute the remainder
+}     
+//END OF THE PROGRAM    
--- Poly.h
+++ Poly.h
@@ -8,12 +8,14 @@ int size;
 
 public:
 Poly();
+Poly(const Poly & ref);
 Poly(double values[],int n);
 int getSize(void) const ;
 
-Poly& operator+(const Poly & opp);
-Poly& operator-(const Poly & opp);
-Poly& operator*(const Poly & opp);
+Poly operator+(const Poly & opp);
+double operator()(const int &i) const;
+Poly operator-(const Poly & opp);
+Poly operator*(const Poly & opp);
 void print(void) const;
 
 };
